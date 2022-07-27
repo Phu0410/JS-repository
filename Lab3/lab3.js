baa8404d5c38943a8e710044241736c7fb4679cb
Phu0410 <donguyenthinhphu04102000@gmail.com>
2022-07-28 06:29:02 +0700

    lab3

---
+++ Lab3/lab3.js
@@ -0,0 +1,11 @@
+// 3.1
+const a = '9' - '5';
+const b = '19' - '13' + '17';
+const c = '19' - '13' + 17;
+const d = '123' < 57; 
+const e= 5 + 6 + '4' + 9 - 4 - 2;
+console.log(a, b, c, d, e);
+// 3.2
+let numNeighbours = prompt  ('How many neighbor countries does your country have?');
+
+
